d34a370a73d95d6f75c7c99f4aba14e5806ed84a
marwaneltary <marwaneltary@gmail.com>
2024-09-15 19:06:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function message(){
+     document.getElementById("text").innerHTML ="marwan";
+}
+
+function changeName(){
+     document.getElementById("name").innerHTML ="mohamed";
+}
+
+function changegame(){
+     document.getElementById("game").innerHTML ="minecraft";
+}
